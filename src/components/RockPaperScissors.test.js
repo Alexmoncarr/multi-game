a4fc62cf04b8012437928465997586ff6b9a7146
Alejandro Montalvo Carrasco <z01q3@ferrovial.com>
2024-11-18 13:19:15 +0100

    Fix RockPaperScissors test to handle multiple matching elements

--- src/components/RockPaperScissors.test.js
+++ src/components/RockPaperScissors.test.js
@@ -8,7 +8,8 @@ test('renders RockPaperScissors component', () => {
 });
 
 test('handles choice click correctly', () => {
-  const { getByText } = render(<RockPaperScissors mode="single" onBackToHome={() => {}} />);
+  const { getByText, getAllByText } = render(<RockPaperScissors mode="single" onBackToHome={() => {}} />);
   fireEvent.click(getByText('Piedra'));
-  expect(getByText(/Jugador 1/i)).toBeInTheDocument();
+  const player1Elements = getAllByText(/Jugador 1/i);
+  expect(player1Elements.length).toBeGreaterThan(0);
 });
