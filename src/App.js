47dbb6bcafb72aede76f91389c24143dde192007
Alejandro Montalvo Carrasco <z01q3@ferrovial.com>
2024-11-19 11:21:40 +0100

    Fix RockPaperScissors navigation and functionality

--- src/App.js
+++ src/App.js
@@ -5,13 +5,16 @@ import GuessNumber from './components/GuessNumber';
 import MemoryGame from './components/MemoryGame';
 import TicTacToe from './components/TicTacToe';
 import Calculator from './components/Calculator';
+import RockPaperScissors from './components/RockPaperScissors';
 import './App.css';
 
 function App() {
   const [game, setGame] = useState(null);
+  const [gameMode, setGameMode] = useState(null);
 
   const handleBackToHome = () => {
     setGame(null);
+    setGameMode(null);
   };
 
   return (
@@ -19,7 +22,8 @@ function App() {
       {!game && <Home setGame={setGame} />}
       {game === 'calculator' && <Calculator onBackToHome={handleBackToHome} />}
       {game === 'tic-tac-toe' && <TicTacToe onBackToHome={handleBackToHome} />}
-      {game === 'rock-paper-scissors' && <HomeRock onBackToHome={handleBackToHome} />}
+      {game === 'rock-paper-scissors' && !gameMode && <HomeRock setGameMode={setGameMode} onBackToHome={handleBackToHome} />}
+      {game === 'rock-paper-scissors' && gameMode && <RockPaperScissors mode={gameMode} onBackToHome={handleBackToHome} />}
       {game === 'guess-number' && <GuessNumber onBackToHome={handleBackToHome} />}
       {game === 'memory-game' && <MemoryGame onBackToHome={handleBackToHome} />}
     </div>
