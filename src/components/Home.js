36981033b7d5effb21f5e9a3c36930e6724fe307
Alejandro Montalvo Carrasco <z01q3@ferrovial.com>
2024-11-19 09:39:41 +0100

    prueba

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ function Home({ setGame }) {
     <div className="home">
       <h1>Selecciona un Juego</h1>
       <button onClick={() => setGame('calculator')}>Calculadora</button>
-      <button onClick={() => setGame('tic-tac-toe')}>Tic Tac Toe</button>
+      <button onClick={() => setGame('tic-tac-toe')}>Tic-Tac-Toe</button>
       <button onClick={() => setGame('rock-paper-scissors')}>Piedra, Papel, Tijera</button>
       <button onClick={() => setGame('guess-number')}>Adivina el Número</button>
       <button onClick={() => setGame('memory-game')}>Memoria</button>
